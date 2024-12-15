b9d6286d42bed4e78a814685c55dbd71aebfdb52
Nimeshis <dhungelnimesh5@gmail.com>
2024-12-15 21:55:43 +0545

    localhost on package json

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-import cors from "cors";
-import jsonServer from "json-server";
+// import cors from "cors";
+// import jsonServer from "json-server";
 
-const server = jsonServer.create();
-const router = jsonServer.router("src/data/db.json"); // Path to your JSON file
-const middlewares = jsonServer.defaults();
+// const server = jsonServer.create();
+// const router = jsonServer.router("src/data/db.json"); // Path to your JSON file
+// const middlewares = jsonServer.defaults();
 
-// Enable CORS with allow-all origins
-server.use(
-  cors({
-    origin: "*", // Allow all origins
-  })
-);
+// // Enable CORS with allow-all origins
+// server.use(
+//   cors({
+//     origin: "*", // Allow all origins
+//   })
+// );
 
-server.use(middlewares);
-server.use(router);
+// server.use(middlewares);
+// server.use(router);
 
-const PORT = 8001;
-const HOST = "0.0.0.0";
+// const PORT = 8001;
+// const HOST = "0.0.0.0";
 
-server.listen(PORT, HOST, () => {
-  console.log(`JSON Server is running at http://${HOST}:${PORT}`);
-});
+// server.listen(PORT, HOST, () => {
+//   console.log(`JSON Server is running at http://${HOST}:${PORT}`);
+// });
